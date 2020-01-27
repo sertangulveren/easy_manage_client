475d423773021cfc4cd367aab703a9793945a05f
Sertan Gülveren <sertangulveren@gmail.com>
2020-01-27 21:05:08 +0300

    Top module created.

--- lib/easy_manage_client.rb
+++ lib/easy_manage_client.rb
@@ -1,6 +1,18 @@
-require "easy_manage_client/version"
+# frozen_string_literal: true
+
+require 'byebug'
+require 'faraday'
+require 'easy_manage_client/version'
+require 'easy_manage_client/configuration'
+require 'easy_manage_client/core'
+require 'easy_manage_client/downloader'
+require 'easy_manage_client/railtie' if defined?(Rails)
 
 module EasyManageClient
   class Error < StandardError; end
-  # Your code goes here...
+  class InvalidResponseContent < Error; end
+  class ProfileNotFound < Error; end
+  class InvalidExtension < Error; end
+  class NoProfilesWereFound < Error; end
+  class DownloadProcessFailed < Error; end
 end
