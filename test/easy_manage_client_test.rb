1253d0b315a7a2f6118fb5442a152323cf6997e3
Sertan Gülveren <sertangulveren@gmail.com>
2020-01-27 21:09:55 +0300

    Top module tests.

--- test/easy_manage_client_test.rb
+++ test/easy_manage_client_test.rb
@@ -1,11 +1,13 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class EasyManageClientTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::EasyManageClient::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_everything_will_be_great
+    assert true
   end
 end
