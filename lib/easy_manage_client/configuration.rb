5c2e40dce1f8d0f9db65a74720346aa3d13b22a5
Sertan Gülveren <sertangulveren@gmail.com>
2020-01-27 21:06:35 +0300

    Configuration structure created.

---
+++ lib/easy_manage_client/configuration.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# main module
+module EasyManageClient
+  class << self
+    attr_accessor :configurations
+
+    def configuration(profile = :default)
+      raise NoProfilesWereFound if configurations.nil? || configurations.empty?
+
+      configurations.select do |conf|
+        conf.profile == profile
+      end.first || raise(ProfileNotFound, "Undefined profile: #{profile}")
+    end
+
+    private
+
+    def reject_profile(profile)
+      configurations.reject! { |conf| conf.profile == profile }
+    end
+
+    def initialize_configuration
+      self.configurations = [] if configurations.nil?
+      Configuration.new
+    end
+
+    def after_configuration_events(conf, profile)
+      conf.profile = profile # FORCE!
+      conf.check!
+      reject_profile(profile)
+      configurations << conf
+    end
+  end
+
+  def self.configure(profile = :default)
+    conf = initialize_configuration
+    yield(conf)
+    after_configuration_events(conf, profile)
+  end
+
+  # Configuration class
+  class Configuration
+    EXTENSION_TYPES = %w[yml json].freeze
+
+    attr_accessor :profile, :root_url, :auth_token, :extension, :compile_id,
+                  :download_folder
+
+    def initialize
+      self.profile = :default
+      self.extension = 'yml'
+    end
+
+    def check!
+      raise InvalidExtension unless EXTENSION_TYPES.include?(extension)
+    end
+  end
+end
