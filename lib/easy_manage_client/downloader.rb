a19f656800569ac245db324630dbfd2951619c7d
Sertan Gülveren <sertangulveren@gmail.com>
2020-01-29 17:46:31 +0300

    Downloader changed to run with new file type definitions.

--- lib/easy_manage_client/downloader.rb
+++ lib/easy_manage_client/downloader.rb
@@ -3,6 +3,7 @@
 module EasyManageClient
   # Downloader
   class Downloader < Core
+    # It decides which api endpoint to use and returns this endpoit.
     def request_uri
       # if version not specified, use latest uri
       if ::EasyManageClient.configuration(profile).compile_id.nil?
@@ -13,46 +14,54 @@ module EasyManageClient
       compiled_version_uri
     end
 
+    # Call api, handle response, perform all actions according to success.
+    # Method returns true or false.
     def perform
       # make api call and handle response
       super
-      # if success is not true
-      # stop operation and return false
+
+      # If success is not true, stop operation and return false.
       return false unless success
 
-      # download data to the relevant location
+      # Download data to the relevant location.
       write_to_folder
       true
     end
 
+    # CRASH! If unsuccessful.
     def perform!
       perform
       unless success
         raise ::EasyManageClient::DownloadProcessFailed, response[:message]
       end
 
+      # Everything is OK.
       true
     end
 
+    # Returns request uri from specified version.
     def compiled_version_uri
       '/api/projects/by_reference/' +
         ::EasyManageClient.configuration(profile).compile_id.to_s
     end
 
+    # Returns request uri for latest version.
     def latest_version_uri
       '/api/projects/latest_compiled_version'
     end
 
+    # Write content to download folder.
     def write_to_folder
       File.open(prepare_file_path_to_download, 'w') do |file|
         file.write(response[:content])
       end
     end
 
+    # Prepares a file path from the specified extension and download folder.
     def prepare_file_path_to_download
       folder = ::EasyManageClient.configuration(profile).download_folder
-      extension = ::EasyManageClient.configuration(profile).extension
-      File.join(folder, "#{response[:reference]}.#{extension}")
+      file_extension = ::EasyManageClient.configuration(profile).file_extension
+      File.join(folder, "#{response[:reference]}.#{file_extension}")
     end
   end
 end
